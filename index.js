6eaf8963fc1fdcaa441d3b84b2f4ac0759aa2113
Abbas Mashayekh <martianboy2005@gmail.com>
2016-09-29 13:46:18 +0330

    Arabic and Persian number sets should be in the same order

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 function persianAlphabeticCompare(s1, s2) {
     var persian_alphabet_fix_map = {
         'ؤ': 1608.5,
-        'ئ': 1609.5,
+        'ئ': 1610,
         'پ': 1577,
         'ة': 1607.5,
+        'ۀ': 1607.5,
         'ژ': 1586.5,
         'ک': 1603,
         'چ': 1580.5,
         'گ': 1603.5,
+        'ی': 1609,
+        'ي': 1609,
+        '۰': 1632,
+        '۱': 1633,
+        '۲': 1634,
+        '۳': 1635,
+        '۴': 1636,
+        '۵': 1637,
+        '۶': 1638,
+        '۷': 1639,
+        '۸': 1640,
+        '۹': 1641
     }
 
     // For simplicity, just ignore the range that includes Arabic diacritics.
@@ -42,7 +55,7 @@ function persianAlphabeticCompare(s1, s2) {
         var cmp_value =
             (persian_alphabet_fix_map[s1[i]] || c1) -
             (persian_alphabet_fix_map[s2[j]] || c2);
-        
+
         if (cmp_value) return cmp_value;
 
         return compareAtIndex(i + 1, j + 1);
