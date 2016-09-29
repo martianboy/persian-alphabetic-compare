1e629eb0d0b53699db80f4a14dda73b6615929c9
Abbas Mashayekh <martianboy2005@gmail.com>
2016-09-29 13:44:52 +0330

    Ignore Arabic diacritics.

--- index.js
+++ index.js
@@ -8,24 +8,47 @@ function persianAlphabeticCompare(s1, s2) {
         'ک': 1603,
         'چ': 1580.5,
         'گ': 1603.5,
-        'ی': 1610
     }
 
-    function compareAtIndex(i) {
-        if (i >= s1.length && i >= s2.length) return 0;
-        if (i >= s1.length) return -1;
-        if (i >= s2.length) return 1;
+    // For simplicity, just ignore the range that includes Arabic diacritics.
+    var ignore_range = [1611, 1631];
+
+    function is_eos(s, i) {
+        if (i >= s.length) return true;
+
+        var c = s.charCodeAt(i);
+        if (c < ignore_range[0] || c > ignore_range[1])
+            return false;
+
+        return is_eos(s, i + 1);
+    }
+
+    function compareAtIndex(i, j) {
+        var eos1 = is_eos(s1, i);
+        var eos2 = is_eos(s2, j);
+
+        if (eos1 && eos2) return 0;
+        if (eos1) return -1;
+        if (eos2) return 1;
+
+        var c1 = s1.charCodeAt(i);
+        if (c1 >= ignore_range[0] && c1 <= ignore_range[1])
+            return compareAtIndex(i + 1, j);
+
+        var c2 = s2.charCodeAt(j);
+        if (c2 >= ignore_range[0] && c2 <= ignore_range[1])
+            return compareAtIndex(i, j + 1);
 
         var cmp_value =
-            (persian_alphabet_fix_map[s1[i]] || s1.charCodeAt(i)) -
-            (persian_alphabet_fix_map[s2[i]] || s2.charCodeAt(i));
+            (persian_alphabet_fix_map[s1[i]] || c1) -
+            (persian_alphabet_fix_map[s2[j]] || c2);
         
-        if (!cmp_value) return compareAtIndex(i + 1);
+        if (cmp_value) return cmp_value;
 
-        return cmp_value;
+        return compareAtIndex(i + 1, j + 1);
     }
 
-    return compareAtIndex(0);
+    return compareAtIndex(0, 0);
 }
 
 module.exports = persianAlphabeticCompare;
